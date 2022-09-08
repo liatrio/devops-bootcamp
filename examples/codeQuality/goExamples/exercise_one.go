32c21a393157f3b1f2f7b7a9d32282edb7392e8e
bserrato7900 <benjamins@liatrio.com>
2022-09-08 09:12:53 -0700

    update: exercise_one.go conforming to unit tests provided

--- examples/codeQuality/goExamples/exercise_one.go
+++ examples/codeQuality/goExamples/exercise_one.go
@@ -1,6 +1,20 @@
 package go_unit_test_bootcamp
 
 func FindMissingDrone(droneIds []int) int {
-    // TODO: implement this
-    return -1
+	unique := droneIds[0]
+	for i, ids := range droneIds{
+		dup := false
+		for j,neighbor := range droneIds{
+			if ids == neighbor{
+				if i != j {
+					dup = true
+					break;
+				}
+			}
+		}		
+		if dup == false {
+			unique = ids
+		}
+	}
+	return unique
 }
