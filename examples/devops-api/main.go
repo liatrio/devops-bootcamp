7ffaa2a2438e34086bb1d56e6c7a3354be064c2f
Steven Mendez <96746466+MinecraftSt3v3@users.noreply.github.com>
2022-10-21 11:55:23 -0700

    added the random id

--- examples/devops-api/main.go
+++ examples/devops-api/main.go
@@ -1,9 +1,11 @@
 package main
 
 import(
-    "os"
-    "fmt"
-    "net/http"
+	"os"
+	"fmt"
+	"net/http"
+	"crypto/rand"
+	"encoding/base32"
 )
 
 func main() {
@@ -11,8 +13,29 @@ func main() {
 	url := protocol + "www.google.com"
 	response, err := http.Get(url)
 	if err != nil { //if theres an error in GET request
-	    fmt.Printf("error in GET request: %s\nErrors: %s\n",url, err)
-	    os.Exit(1)
+		fmt.Printf("error in GET request: %s\nErrors: %s\n",url, err)
+		os.Exit(1)
 	}
 	fmt.Println(response)
+
+	// random number generator	
+	//rand.Seed(time.Now().UnixNano())
+	//min := 10
+	//max := 30
+
+
+	//fmt.Println(rand.Intn(max - min + 1) + min)
+
+	randId := getRandId(15)
+	fmt.Println("Random ID: ", randId)
+
+}
+
+func getRandId(length int) string {
+    randomBytes := make([]byte, 32)
+    _, err := rand.Read(randomBytes)
+    if err != nil {
+        panic(err)
+    }
+    return base32.StdEncoding.EncodeToString(randomBytes)[:length]
 }
