910cb2f1c98e6867c99ae6be3a7699654cf2a201
Evan Drake <evanmdrake@icloud.com>
2022-10-21 16:05:58 -0700

    fixed: formating issues , typing issues

--- examples/devops-api/main.go
+++ examples/devops-api/main.go
@@ -1,73 +1,55 @@
 package main
 
-import(
-	"os"
-	"fmt"
-	"net/http"
+import (
 	"crypto/rand"
 	"encoding/base32"
+	"fmt"
+	"net/http"
+	"os"
 )
 
-
-
-type engineer struct{
-	name string
-	id string
+type engineer struct {
+	name  string
+	id    string
 	email string
 }
 
-type dev struct{
-	name string
-	id string
+type dev struct {
+	name      string
+	id        string
 	engineers []string
 }
 
-type ops struct{
-	name string
-	id string
+type ops struct {
+	name      string
+	id        string
 	engineers []string
 }
 
-type devops struct{
-	id string
+type devops struct {
+	id  string
 	dev []string
 	ops []string
 }
-//Global maps to access our resources by id key
-engineers := make(map[string] engineer)
-devs := make(map[string] devs)
-ops := make(map[string] ops)
-devops := make(map[string] devops)
 
-func newDev (name string, email string) dev{
-	devGroup := dev {name: name}
+// Global maps to access our resources by id key
+var engineers = make(map[string]engineer)
+var developers = make(map[string]dev)
+var operations = make(map[string]ops)
+var developer_operations = make(map[string]devops)
+
+func newDev(name string, email string) dev {
+	devGroup := dev{name: name}
 	devGroup.id = "TODO"
-	devGroup.email = email
-	devGroup.engineers = make([]engineer,1)
+	devGroup.engineers = make([]string, 1)
 	return devGroup
 }
 
-
-func newPerson (name string) *engineer {
-	p := engineer {name: name}
-	p.id := "123"
+func newPerson(name string) *engineer {
+	p := engineer{name: name}
+	p.id = "123"
 	p.email = "asdasd@gmail.com"
 	return &p
-}
-
-func main() {
-	protocol := "http://"
-	url := protocol + "www.google.com"
-	response, err := http.Get(url)
-	if err != nil { //if theres an error in GET request
-		fmt.Printf("error in GET request: %s\nErrors: %s\n",url, err)
-		os.Exit(1)
-	}
-	fmt.Println(response)
-
-	randId := getRandId(15)
-//	fmt.Println("Random ID: ", randId)
-	fmt.Println(engineer{"bob",randId ,"hello@gmail.com"})
 }
 
 func getRandId(length int) string {
@@ -77,4 +59,19 @@ func getRandId(length int) string {
 		panic(err)
 	}
 	return base32.StdEncoding.EncodeToString(randomBytes)[:length]
+}
+
+func main() {
+	protocol := "http://"
+	url := protocol + "www.google.com"
+	response, err := http.Get(url)
+	if err != nil { //if theres an error in GET request
+		fmt.Printf("error in GET request: %s\nErrors: %s\n", url, err)
+		os.Exit(1)
+	}
+	fmt.Println(response)
+
+	randId := getRandId(15)
+	//	fmt.Println("Random ID: ", randId)
+	fmt.Println(engineer{"bob", randId, "hello@gmail.com"})
 }
