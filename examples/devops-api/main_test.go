2b4eda1d658150eb923f0c5363ebebddb085d6e5
baserrato <benjamins@liatrio.com>
2022-10-24 08:15:58 -0700

    test: more cases for email verification

--- examples/devops-api/main_test.go
+++ examples/devops-api/main_test.go
@@ -9,13 +9,16 @@ type emailTest struct {
 
 var verifyEmailTests = []emailTest{
 	emailTest{"bob@bob.com", true},
+	emailTest{"b0b123@clever.ask.who", true},
+	emailTest{"bob#@bob.com", false},
+	emailTest{"bob@bob", false},
 }
 
-func testVerifyEmail(t *testing.T) {
+func TestVerifyEmail(t *testing.T) {
 	for _, test := range verifyEmailTests {
 		result := verifyEmail(test.email)
 		if result != test.expected {
-			t.Errorf("Expected: %t, Received: %t", test.expected, result)
+			t.Errorf("Email: %s\nExpected: %t, Received: %t", test.email, test.expected, result)
 		}
 	}
 }
