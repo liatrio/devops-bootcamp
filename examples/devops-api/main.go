d1436a335cdbc09279dd6c5b7dc447660548ac96
Steven Mendez <96746466+MinecraftSt3v3@users.noreply.github.com>
2022-10-25 08:55:11 -0700

    adding gin functionality

--- examples/devops-api/main.go
+++ examples/devops-api/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"crypto/rand"
 	"encoding/base32"
-	"errors"
+	//"errors"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
 	"regexp"
@@ -19,99 +20,111 @@ func getRandId(length int) string {
 	return base32.StdEncoding.EncodeToString(randomBytes)[:length]
 }
 
+
 type engineer struct {
-	name  string
-	id    string
-	email string
+	Name  string `json:"name"`
+	Id    string `json:"id"`
+	Email string `json:"email"`
 }
 
-type dev struct {
-	name      string
-	id        string
-	engineers map[string]engineer
+var engineers = []engineer{
+
+	{Name: "Bobs Burgers", Id: "12", Email: "bob@gmail.com"},
+	{Name: "Alice Joice", Id: "1", Email: "alice@gmail.com"},
+	{Name: "Benjamin Serrato", Id: "2", Email: "benny@gmail.com"},
 }
 
-type ops struct {
-	name      string
-	id        string
-	engineers map[string]engineer
+func getEngineer(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, engineers)
 }
 
-type devops struct {
-	id  string
-	dev map[string]dev
-	ops map[string]ops
-}
+// type dev struct {
+// 	name      string
+// 	id        string
+// 	engineers map[string]engineer
+// }
+
+// type ops struct {
+// 	name      string
+// 	id        string
+// 	engineers map[string]engineer
+// }
+
+// type devops struct {
+// 	id  string
+// 	dev map[string]dev
+// 	ops map[string]ops
+// }
 
 // Global maps to access our resources by id key
-var engineers = make(map[string]engineer)
-var developers = make(map[string]dev)
-var operations = make(map[string]ops)
-var developer_operations = make(map[string]devops)
+//var engineers = make(map[string]engineer)
+// var developers = make(map[string]dev)
+// var operations = make(map[string]ops)
+// var developer_operations = make(map[string]devops)
 
-func newDevOps() (devops, error) {
-	devOpsGroup := devops{id: "TODO"}
-	devOpsGroup.ops = make(map[string]ops)
-	devOpsGroup.dev = make(map[string]dev)
-	developer_operations[devOpsGroup.id] = devOpsGroup
-	return devOpsGroup, nil
-}
+// func newDevOps() (devops, error) {
+// 	devOpsGroup := devops{id: "TODO"}
+// 	devOpsGroup.ops = make(map[string]ops)
+// 	devOpsGroup.dev = make(map[string]dev)
+// 	developer_operations[devOpsGroup.id] = devOpsGroup
+// 	return devOpsGroup, nil
+// }
 
-func newDev(name string) (dev, error) {
-	for _, value := range developers {
-		if name == value.name {
-			return dev{}, errors.New(" Dev group already exists ")
-		}
-	}
-	devGroup := dev{name: name, id: "TODO"}
-	devGroup.engineers = make(map[string]engineer)
-	developers[devGroup.id] = devGroup
-	return devGroup, nil
-}
+// func newDev(name string) (dev, error) {
+// 	for _, value := range developers {
+// 		if name == value.name {
+// 			return dev{}, errors.New(" Dev group already exists ")
+// 		}
+// 	}
+// 	devGroup := dev{name: name, id: "TODO"}
+// 	devGroup.engineers = make(map[string]engineer)
+// 	developers[devGroup.id] = devGroup
+// 	return devGroup, nil
+// }
 
-func newOp(name string) (ops, error) {
-	for _, value := range operations {
-		if name == value.name {
-			return ops{}, errors.New(" Operations group already exists ")
-		}
-	}
-	opGroup := ops{name: name, id: "TODO"}
-	opGroup.engineers = make(map[string]engineer)
-	operations[opGroup.id] = opGroup
-	return opGroup, nil
-}
+// func newOp(name string) (ops, error) {
+// 	for _, value := range operations {
+// 		if name == value.name {
+// 			return ops{}, errors.New(" Operations group already exists ")
+// 		}
+// 	}
+// 	opGroup := ops{name: name, id: "TODO"}
+// 	opGroup.engineers = make(map[string]engineer)
+// 	operations[opGroup.id] = opGroup
+// 	return opGroup, nil
+// }
 
-func newPerson(name string, email string) (engineer, error) {
-	for _, value := range engineers {
-		if name == value.name {
-			return engineer{}, errors.New(" Engineer already exists ")
-		}
-	}
-	p := engineer{name: name, id: getRandId(5)}
-	p.email = email
-	engineers[p.id] = p
-	return p, nil
-}
+// func newPerson(name string, email string) (engineer, error) {
+// 	for _, value := range engineers {
+// 		if name == value.name {
+// 			return engineer{}, errors.New(" Engineer already exists ")
+// 		}
+// 	}
+// 	p := engineer{name: name, id: getRandId(5)}
+// 	p.email = email
+// 	//engineers[p.id] = p
+// 	return p, nil
+// }
 
-func addEngineerTo_Op(op_id string, engineer_id string) (bool, error) {
+// func addEngineerTo_Op(op_id string, engineer_id string) (bool, error) {
 
-	engineer_val, exists := engineers[engineer_id]
-	if !exists {
-		return false, errors.New(" Engineer doesn't exist ")
-	}
-	dev_val, exists := developers[op_id]
-	if !exists {
-		return false, errors.New(" Operations group doesn't exist ")
-	}
-	_, exists = dev_val.engineers[engineer_id]
-	if exists {
-		return false, errors.New(" Engineer already exists inside specified Operations group ")
-	}
-	dev_val.engineers[engineer_id] = engineer_val
+// 	engineer_val, exists := engineers[engineer_id]
+// 	if !exists {
+// 		return false, errors.New(" Engineer doesn't exist ")
+// 	}
+// 	dev_val, exists := developers[op_id]
+// 	if !exists {
+// 		return false, errors.New(" Operations group doesn't exist ")
+// 	}
+// 	_, exists = dev_val.engineers[engineer_id]
+// 	if exists {
+// 		return false, errors.New(" Engineer already exists inside specified Operations group ")
+// 	}
+// 	dev_val.engineers[engineer_id] = engineer_val
 
-	return true, nil
+// 	return true, nil
 
-}
+// }
 
 func verifyEmail(email string) bool {
 	result, _ := regexp.MatchString("^([a-z]|[0-9])+@[a-z]+\\.[a-z]+(\\.[a-z]+)*$", email)
@@ -126,6 +139,12 @@ func main() {
 		fmt.Printf("error in GET request: %s\nErrors: %s\n", url, err)
 		os.Exit(1)
 	}
+
+	router := gin.Default()
+	router.GET("/engineers", getEngineer)
+
+	router.Run("localhost:8080")
+
 	fmt.Println(response)
 
 	randId := getRandId(15)
