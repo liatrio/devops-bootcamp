cea43d2f27c1f638bb93fe42c6d0b7d60ef224c6
baserrato <benjamins@liatrio.com>
2022-12-06 16:02:37 -0800

    fix: devops script issues

--- examples/ch6/devops-api/scripts/devops.sh
+++ examples/ch6/devops-api/scripts/devops.sh
@@ -19,12 +19,12 @@ elif [[ $1 == "delete" ]]; then
   exit
 # Gets all DevOps resources or a specific DevOps resource by Id using GET request
 # Arguments to get all DevOps resources: 1: get
-# Arguments to get a specific DevOps resource: 1: get, 2: type: id, 3: actual id
-elif [[ $1 == "get"]]; then
+# Arguments to get a specific DevOps resource: 1: get, 2: id
+elif [[ $1 == "get" ]]; then
   if [[ $# == 1 ]]; then
 	  curl http://localhost:8080/devops
-  elif [[ $# == 3 ]]; then
-    curl http://localhost:8080/devops/${2}/${3}
+  elif [[ $# == 2 ]]; then
+    curl http://localhost:8080/devops/${2}
   fi
   exit
 fi
@@ -39,7 +39,7 @@ echo delete [devops id] "<-" deletes DevOps resource
 echo 
 echo get "<-" gets all DevOps resources
 echo 
-echo get [type: id] [devops id] "<-" gets specific DevOps resource from id
-echo -e '   ' - [type: id] should be the word "id".
+echo get [devops id] "<-" gets specific DevOps resource from id
 echo -e '   ' - [devops id] enter the id of the devops resource.
+echo
 
