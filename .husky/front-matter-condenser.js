951da9c169e5649b8588c367fe26d254ea09dca6
Joshua Burns <joshua.burns@liatrio.com>
2023-04-28 16:08:02 -0700

    Cleans up debugging code adds color to output

--- .husky/front-matter-condenser.js
+++ .husky/front-matter-condenser.js
@@ -2,10 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const simpleGit = require('simple-git');
 const matter = require('gray-matter');
-const YAML = require('json-to-pretty-yaml');
 const { Console } = require('console');
 
 const git = simpleGit();
+const green = '\x1b[32m';
+const reset = '\x1b[0m';
+const yellow = '\x1b[33m';
+
 
 const MASTER_RECORD_PATH = path.join(__dirname, '../docs/README.md');
 
@@ -37,17 +40,9 @@ async function main() {
             const fileContent = fs.readFileSync(mdFile, 'utf8');
 
             // Parse the YAML front matter.
-            // const exerciseData = matter(fileContent).data;
             const parsedExercise = matter(fileContent);
 
-            // Check if we deleted a section.
-
             const exerciseData = parsedExercise.data
-
-            console.log(exerciseData);
-            // Need to handle the delete case. When an exercise section is deleted we should
-            // Remove the whole section from the master record
-            // When
 
             // merge the front-matter from the staged file into the master record.
             // This will overwrite the inner object on the master record if it exists
@@ -56,18 +51,10 @@ async function main() {
                 data = { ...data, ...exerciseData };
                 masterRecordChanged = true;
             }
-            console.log(data);
-
-            //   // START HERE I want to modify this logic so that if the key of data is included in masterRecord then update
-            //   // masterRecord. If not then push all of data into masterRecord
-            //   // If the file has front matter and it's not in the master record, add it.
-            //   if (data && !masterRecord.some(entry => JSON.stringify(entry) === JSON.stringify(data))) {
-            //     masterRecord.push(data);
-            //     masterRecordChanged = true;
-            //   }
         }
 
-        // chatgpt code to sort object by key
+        // Create an array of key-value pairs and then sorts them alphabetically
+        // by the keys.
         const sortedEntries = Object.entries(data).sort(
             (a, b) => a[0].localeCompare(b[0])
         );
@@ -78,30 +65,29 @@ async function main() {
         // Create a new object from the sorted entries
         const sortedData = Object.fromEntries(sortedEntries);
         const sortedMasterRecord = Object.fromEntries(sortedMasterEntries);
-        // end of chatgpt code
 
+        // Stringify the object and compare. This handles the case of an .md file being updated
+        // but not the front-data
         if (JSON.stringify(sortedData) === JSON.stringify(sortedMasterRecord)) {
             masterRecordChanged = false;
         }
 
-        //console.log(Object.keys(masterRecord));
-        //console.log(YAML.stringify(JSON.parse(fs.readFileSync('/Users/jburns/git/devops-bootcamp/docs/1-introduction/1.1-devops-defined.json', 'utf-8'))))
-        const updatedContent = matter.stringify(content, data);
-        // console.log(updatedContent)
-
-
         if (masterRecordChanged) {
-            // We can always write the content back to the master record because git does not care about timestamps
-            // So we have three cases, no .md files were staged, and .md
+            // Stringify the updated front-matter and origional content
+            const updatedContent = matter.stringify(content, sortedData);
+
+            // Update the master record with the new front-matter. Use the sorted
             fs.writeFileSync(MASTER_RECORD_PATH, updatedContent);
 
-            console.error('New front matter detected, master record updated. Please review changes to ./docs/README.md');
+            console.error(`${yellow}New front matter detected, master record updated${reset}`);
+            console.error(`${yellow}Please review changes to ./docs/README.md${reset}`);
             process.exit(1);
         } else {
             // Continue with the commit.
-            console.log('No changes to master record, proceeding with commit.');
+            console.log(`\n\n${green}No changes to master record, proceeding with commit.${reset}`);
             process.exit(0);
         }
+
     } catch (error) {
         console.error('An error occurred:', error);
         process.exit(1);
