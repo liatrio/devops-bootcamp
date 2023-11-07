6935efeb5cbc699d4e3a3589582c095d04a7859d
Mikayla Billings-Alston <mikayla.billings-alston@liatrio.com>
2023-11-06 16:06:54 -0800

    Added quizzes to 2.2 and 2.5

---
+++ src/quizzes/2-Github/secrets.js
@@ -0,0 +1,25 @@
+let rawQuizdown = `
+---
+primaryColor: '#24ae1dff'
+shuffleAnswers: true
+---
+# What does CodeQL do?
+
+- [x] Scan your code for security vulnerabilities
+- [ ] Scan your code for sensitive information, like tokens
+- [ ] Scan your code for outdated pacakges
+
+# What does dependabot do?
+
+- [x] Open pull requests in your repository when it detects you're using an outdated, vulnerable package
+- [ ] Open pull requests in your repository when it detects you've committed sensitive information
+- [ ] Open pull requests in your repository when it detects your code has vulnerabilities
+
+# What does secret scanning do?
+
+- [x] Email you notifications when it detects secrets have been committed to your repository
+- [ ] Email you notifications when it detects your code has vulnerabilities
+- [ ] Email you notifications when it detects you're using outdated packages
+`
+
+export { rawQuizdown }
---
+++ src/quizzes/2-Github/workflow.js
@@ -0,0 +1,26 @@
+let rawQuizdown = `
+---
+primaryColor: '#24ae1dff'
+shuffleAnswers: true
+---
+# What does 'on' do?
+
+- [ ] Specifies what kind of virtual machine the action will be run on
+- [x] Specifies what will trigger this workflow to run
+- [ ] Specifies the name of the workflow that will appear in the "Actions" tab of your repo
+
+# What does 'jobs' do?
+- [ ] Group together all of the steps of the workflow
+- [x] Group together all of the jobs of the workflow
+- [ ] Specify the name of the jobs in your workflow
+
+# What does 'uses' do?
+- [ ] Specifies the command to be run for this step
+- [x] Specifies the action to be run for this step
+
+# What does 'run' do?
+- [x] Specifies the command to be run for this step
+- [ ] Specifies the action to be run for this step
+`
+
+export { rawQuizdown }
