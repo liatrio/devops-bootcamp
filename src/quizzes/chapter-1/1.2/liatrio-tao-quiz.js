0c86e11fc66f64169c0e0a9742fa2916f79ef212
Joshua <joshua.burns@liatrio.com>
2023-12-22 09:21:17 -0800

    Apply suggestions from code review

--- src/quizzes/chapter-1/1.2/liatrio-tao-quiz.js
+++ src/quizzes/chapter-1/1.2/liatrio-tao-quiz.js
@@ -18,7 +18,7 @@ shuffleAnswers: true
 # Our end goal for each project (whether it's our own or our client's) is to completely finish it
 
 1. [ ] True
-  > Are projects really ever *finished*? Take a look at the *Continuous Improvement* field of the Tao
+  > Are projects really ever *finished*? Take a look at the *Continuous Improvement* section of the Tao
 1. [x] False
 
 # What is the most important area of focus in a project in terms of delivery improvement?
@@ -32,7 +32,7 @@ shuffleAnswers: true
 # Throughout the lifecycle of a project, we do our best to avoid any bugs, errors or flaws as not to break things
 
 1. [ ] True
-	> Failure is part of the process! It's bound to happen at some point, so it's important to have a strong culture around it for when it does. Check out the *Culture of Feedback* field of the Tao for more.
+	> Failure is part of the process! It's bound to happen at some point, so it's important to have a strong culture around it for when it does. Check out the *Culture of Feedback* section of the Tao for more.
 2. [x] False
 `;
 
