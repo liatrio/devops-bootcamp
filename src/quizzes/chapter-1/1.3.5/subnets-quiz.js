2b4d556ff2d5b0dc76de3a604193381c31cc10e4
cfculler <carsonculler2020@gmail.com>
2023-12-22 11:15:54 -0800

    Added quiz about network addresses and subnets

---
+++ src/quizzes/chapter-1/1.3.5/subnets-quiz.js
@@ -0,0 +1,31 @@
+const rawQuizdown = `
+---
+shuffleAnswers: true
+---
+
+# Select all that will fall in the network address \`110.5.50.27/8\`
+
+- [x] 110.10.15.20
+- [x] 110.0.100.50
+- [x] 110.5.50.25
+- [ ] 132.20.15.100
+- [ ] 55.28.13.155
+
+# Select all that will fall in the network address \`110.5.50.27/16\`
+
+- [x] 110.5.15.20
+- [x] 110.5.100.50
+- [x] 110.5.50.25
+- [ ] 110.20.15.100
+- [ ] 55.28.13.155
+
+# Select all that will fall in the network address \`110.5.50.27/24\`
+
+- [x] 110.5.50.20
+- [x] 110.5.50.25
+- [ ] 110.5.100.50
+- [ ] 110.20.50.100
+- [ ] 55.5.50.100
+`;
+
+export { rawQuizdown }
